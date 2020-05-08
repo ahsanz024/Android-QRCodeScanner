bde753305ac385abd2cc5992e13d442e6a4a1f49
Ahsan <ahsan024@gmail.com>
2020-05-08 18:37:43 +0200

    add: QR code detection via bitmap

--- app/src/main/java/com/example/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.kt
@@ -5,15 +5,34 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.util.SparseArray
 import android.view.View
+import androidx.core.util.valueIterator
+import com.google.android.gms.vision.Frame
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
-    val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var detector: BarcodeDetector
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+        private val TAG = MainActivity::class.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buildQrDetector()
+    }
+
+    private fun buildQrDetector() {
+        detector = BarcodeDetector.Builder(this)
+            .setBarcodeFormats(Barcode.QR_CODE)
+//            .setBarcodeFormats(Barcode.ALL_FORMATS)
+            .build()
     }
 
     fun startCamera(v: View) {
@@ -32,6 +51,21 @@ class MainActivity : Activity() {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
             imageView.setImageBitmap(imageBitmap)
+            detectQrCode(imageBitmap)
+        }
+    }
+
+    private fun detectQrCode(image: Bitmap) {
+        if(!detector.isOperational){
+            Log.e(TAG, "Could not setup detector")
+            textView.text = "Could not set up the detector!";
+            return;
+        }
+        val frame: Frame = Frame.Builder().setBitmap(image).build()
+        val barcodes: SparseArray<Barcode> = detector.detect(frame)
+        barcodes.valueIterator().forEach { barcode ->
+            Log.e(TAG, "QRCode result ${barcode.rawValue}")
+            textView.text = barcode.rawValue
         }
     }
 }
